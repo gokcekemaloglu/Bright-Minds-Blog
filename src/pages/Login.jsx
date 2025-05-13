2f0ebdb7a053ecc03d0c3ff8044663704f8c77c8
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2025-05-13 22:10:14 +0300

    feat: Add password visibility toggle to LoginForm component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,7 +23,9 @@ const Login = () => {
           p: 2,
         }}
       >
-        <Grid item xs={12} sm={10} md={6}>
+        <Grid size={{ xs: 12, sm: 10, md: 6 }}
+        //  xs={12} sm={10} md={6}
+         >
           <Avatar
             sx={{
               backgroundColor: "secondary.light",
